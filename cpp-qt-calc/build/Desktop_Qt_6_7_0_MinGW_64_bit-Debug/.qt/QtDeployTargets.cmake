ac1e9a14d4ed791deb20231501f5dd83cfde7ed9
Alfie Fairbank <109432346+Zentifyyy@users.noreply.github.com>
2024-05-15 01:49:48 +0100

    Delete cpp-qt-calc/build directory
    idk what this is LOL

--- cpp-qt-calc/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cpp-qt-calc_FILE C:/Users/alfie/Desktop/Projects/Code/cpp/cpp-qt-calc/cpp-qt-calc/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/cpp-qt-calc.exe)
-set(__QT_DEPLOY_TARGET_cpp-qt-calc_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
